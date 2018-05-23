3a2e109673cfa55c651b27e378c164691a5ba5f0
Deewan Singh <37241176+dwnsingh@users.noreply.github.com>
2018-05-23 20:52:23 +0530

    Update logout.php

--- login/logout.php
+++ login/logout.php
@@ -1,6 +1,11 @@
 <?php 
 session_start(); //to ensure you are using same session
-session_destroy(); //destroy the session
+if(isset($_SESSION['login_user'])){
+	session_unset();
+	session_destroy();
+	$_SESSION['login_user']=NULL;
+	$_SESSION['usertype'] = NULL;
+}
 header("location: ../index.php");//to redirect back to "index.php" after logging out
 exit();
 ?>
